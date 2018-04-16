5597973965b8397d43b198c0314591e1cc069530
markcarrmusic@gmail.com <markcarrmusic@gmail.com>
2018-04-16 20:16:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // Recipe Object
 var recipes = {};
 
+// Update object without mutating
 function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, { key: value});
 }
+
+function
